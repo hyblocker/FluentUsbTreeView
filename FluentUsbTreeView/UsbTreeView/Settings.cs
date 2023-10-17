57b8f5deae2e5c0d37e511fd6621df812ed9b803
Hyblocker <hyblocker@protonmail.com>
2023-10-17 15:42:24 +0200

    Settings singleton stub

---
+++ FluentUsbTreeView/UsbTreeView/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentUsbTreeView.UsbTreeView {
+    public class Settings {
+        private static Settings s_settings;
+        public static Settings Instance {
+            get {
+                if ( s_settings == null) {
+                    s_settings = new Settings();
+                }
+                return s_settings;
+            }
+        }
+
+        public string SettingsPath { get {  return "idk yet lol"; } }
+
+        private Settings() {
+
+        }
+    }
+}
