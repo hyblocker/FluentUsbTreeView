3ec440f55294b0758f7f50fcc4e66e502212fb31
Hyblocker <hyblocker@protonmail.com>
2024-02-05 00:58:01 +0100

    Settings stub

--- FluentUsbTreeView/UsbTreeView/Settings.cs
+++ FluentUsbTreeView/UsbTreeView/Settings.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FluentUsbTreeView.UsbTreeView {
     public class Settings {
@@ -19,7 +20,21 @@ namespace FluentUsbTreeView.UsbTreeView {
         public string SettingsPath { get {  return "idk yet lol"; } }
 
         private Settings() {
-
+            // @TODO: Load settings
         }
+
+        public bool AutoRefresh { get; set; } = true;
+        public bool EndpointDescriptors { get; set; } = true;
+        public bool ScanAllStringDescriptors { get; set; } = false;
+        public bool DescriptorHexDumps { get; set; } = false;
+        public bool DriveNumbersInTree { get; set; } = false;
+        public bool EndpointsInTree { get; set; } = false;
+
+        // Automatic expansion
+        public bool ExpandForEmptyPorts { get; set; } = false;
+        public bool ExpandForEmptyHubs { get; set; } = false;
+        public bool ExpandForNewDevices { get; set; } = true;
+        public bool JumpToNewDevices { get; set; } = false;
+        public bool JumpToRemovedDevices { get; set; } = false;
     }
 }
