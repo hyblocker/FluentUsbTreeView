c176d1f7c51c94e7b4d7d66d66d7c1d6a1c20add
Hyblocker <hyblocker@protonmail.com>
2024-02-05 00:56:49 +0100

    Handle startup and shutdown

--- FluentUsbTreeView/App.xaml.cs
+++ FluentUsbTreeView/App.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using FluentUsbTreeView.PInvoke;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -11,7 +13,38 @@ namespace FluentUsbTreeView {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+
+        public static string[] Arguments;
+
+        // Register device update notifications
+
         private void Application_Startup(object sender, StartupEventArgs e) {
+
+            Arguments = e.Args;
+
+            // Init console ; enables ANSI, unicode, and enables logging in WPF
+            Kernel32.AttachConsole(-1);
+            Console.OutputEncoding = Encoding.Unicode;
+            Kernel32.EnableAnsiCmd();
+
+            Logger.Info($"Received arguments: \"{string.Join(" ", Arguments)}\"");
+
+
+            DeviceManaged.RegisterDeviceNotificationHandler();
+            AppDomain.CurrentDomain.ProcessExit += (s, ev) => DeviceManaged.UnregisterDeviceNotifications();
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e) {
+            DeviceManaged.UnregisterDeviceNotifications();
+        }
+
+        protected override void OnExit(ExitEventArgs e) {
+            DeviceManaged.UnregisterDeviceNotifications();
+            base.OnExit(e);
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e) {
+            Logger.Fatal($"{e.Exception.Message} ({e.Exception.HResult})\nStacktrace: {e.Exception.StackTrace}");
         }
     }
 }
